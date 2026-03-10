b0ecadce654186cd3056089c95a26faebc0b4066
dede79 <limahawkins@gmail.com>
2026-03-10 22:05:38 +0000

    reverting as something broke in urls.py

--- myPortfolio/urls.py
+++ myPortfolio/urls.py
@@ -1,11 +1,10 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
-
+from django.conf.urls.static import static
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-
 from search import views as search_views
 
 urlpatterns = [
@@ -15,22 +14,12 @@ urlpatterns = [
     path("search/", search_views.search, name="search"),
 ]
 
-
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     urlpatterns += staticfiles_urlpatterns()
 
-# Always serve media files regardless of DEBUG mode
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
     path("", include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
 ]
