5f2d90ddfc30fb5153f86cbe7d3fbfff92a5e052
dede79 <limahawkins@gmail.com>
2026-03-12 23:39:18 +0000

    added cloudinary variables

--- myPortfolio/settings/production.py
+++ myPortfolio/settings/production.py
@@ -6,20 +6,29 @@ DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')]
 
-# Database — uses Render's PostgreSQL
+# Database
 DATABASES = {
-    'default': dj_database_url.config(
-        conn_max_age=600,
-    )
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 # Security
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# Whitenoise serves your static files (CSS/JS)
+# Whitenoise for static files (CSS/JS)
 MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware'] + MIDDLEWARE
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Media files (uploaded images/docs)
+# Cloudinary for media files (uploaded images/docs)
+INSTALLED_APPS = INSTALLED_APPS + [
+    'cloudinary',
+    'cloudinary_storage',
+]
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY':    os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/opt/render/project/src'
--- myPortfolio/urls.py
+++ myPortfolio/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
-from django.conf.urls.static import static
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -15,10 +14,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     urlpatterns += staticfiles_urlpatterns()
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = urlpatterns + [
     path("", include(wagtail_urls)),
