62fee8b7bb611157d2b21f07f123963756e62fe5
dede79 <limahawkins@gmail.com>
2026-03-13 00:34:15 +0000

    Fix Cloudinary app order in INSTALLED_APPS2

--- myPortfolio/settings/base.py
+++ myPortfolio/settings/base.py
@@ -45,10 +45,10 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "cloudinary",
-    "cloudinary_storage", 
+    "cloudinary_storage",           
     "django.contrib.staticfiles",
-    "django.contrib.postgres"
+    "cloudinary",                   
+    "django.contrib.postgres",
 ]
 
 MIDDLEWARE = [
--- myPortfolio/settings/production.py
+++ myPortfolio/settings/production.py
@@ -1,38 +1,20 @@
 import os
 import dj_database_url
 import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 from .base import *
 
 DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')]
 
-# Database
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600)
 }
 
-# Security
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# Whitenoise for static files (CSS/JS)
 MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware'] + MIDDLEWARE
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY':    os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
-
-cloudinary.config(
-    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
-    secure=True
-)
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
